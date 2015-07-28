6d9c755bef7369b5343a3427129ca3d475527ebe
Liut <liutao@liut.cc>
2015-07-28 19:00:40 +0800

    add missing file

---
+++ backends/db.go
@@ -0,0 +1,52 @@
+package backends
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	. "tuluu.com/liut/staffio/settings"
+)
+
+var (
+	dbc *sql.DB
+)
+
+func openDb() *sql.DB {
+	db, err := sql.Open("postgres", Settings.Backend.DSN)
+	if err != nil {
+		log.Fatalf("open db error: %s", err)
+	}
+	return db
+}
+
+func getDb() *sql.DB {
+	if dbc == nil {
+		dbc = openDb()
+		return dbc
+	}
+
+	if err := dbc.Ping(); err != nil {
+		dbc = openDb()
+	}
+
+	return dbc
+}
+
+func withTxQuery(query func(tx *sql.Tx) error) error {
+
+	db := getDb()
+	defer db.Close()
+
+	tx, err := db.Begin()
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := query(tx); err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+	return nil
+}
