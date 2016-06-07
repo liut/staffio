3e634f4a9f91d51153ff6ff355cb04057310944f
Liut <liutao@licaigc.com>
2016-06-07 14:21:45 +0800

    simplicity settings

--- settings/config.go
+++ settings/config.go
@@ -2,10 +2,8 @@ package config
 
 import (
 	"flag"
-	"fmt"
-	"log"
-	"os"
-	"strings"
+
+	envcfg "github.com/wealthworks/envflagset"
 )
 
 type config struct {
@@ -37,21 +35,15 @@ type config struct {
 }
 
 var (
-	_configFile  string
-	_loaded      bool
-	fs           *flag.FlagSet
-	Settings     *config = &config{}
-	printVersion bool
+	fs       *flag.FlagSet
+	Settings *config = &config{}
 )
 
 func init() {
 	Settings.Name = "staffio"
 	Settings.Version = buildVersion
-	// Settings.HttpListen = "localhost:3000"
-	// Settings.ResUrl = "/static/"
-	// Settings.LDAP.Host = "localhost"
 
-	fs = flag.NewFlagSet("staffio", flag.ExitOnError)
+	fs = envcfg.New(Settings.Name, Settings.Version)
 
 	fs.StringVar(&Settings.LDAP.Host, "ldap-host", "ldap://localhost:389", "ldap hostname")
 	fs.StringVar(&Settings.LDAP.Base, "ldap-base", "", "ldap base")
@@ -69,63 +61,9 @@ func init() {
 	fs.StringVar(&Settings.SentryDSN, "sentry-dsn", "", "SENTRY_DSN")
 	fs.StringVar(&Settings.Root, "root", "./", "app root directory")
 	fs.BoolVar(&Settings.Debug, "debug", false, "app in debug mode")
-	fs.BoolVar(&printVersion, "version", false, "Print the version and exit")
 
 }
 
 func (c *config) Parse() {
-	perr := fs.Parse(os.Args[1:])
-	switch perr {
-	case nil:
-	case flag.ErrHelp:
-		os.Exit(0)
-	default:
-		os.Exit(2)
-	}
-	if len(fs.Args()) != 0 {
-		log.Fatalf("'%s' is not a valid flag", fs.Arg(0))
-	}
-
-	if printVersion {
-		fmt.Println("staffio version", Settings.Version)
-		os.Exit(0)
-	}
-
-	err := SetFlagsFromEnv(fs, Settings.Name+"_")
-	if err != nil {
-		log.Fatalf("staffio: %v", err)
-	}
-
-}
-
-// SetFlagsFromEnv parses all registered flags in the given flagset,
-// and if they are not already set it attempts to set their values from
-// environment variables. Environment variables take the name of the flag but
-// are UPPERCASE, have the prefix "PREFIX_", and any dashes are replaced by
-// underscores - for example: some-flag => PREFIX_SOME_FLAG
-func SetFlagsFromEnv(fs *flag.FlagSet, prefix string) error {
-	var err error
-	alreadySet := make(map[string]bool)
-	fs.Visit(func(f *flag.Flag) {
-		alreadySet[f.Name] = true
-	})
-
-	if prefix == "" {
-		prefix = "_"
-	} else {
-		prefix = strings.ToUpper(strings.Replace(prefix, "-", "_", -1))
-	}
-
-	fs.VisitAll(func(f *flag.Flag) {
-		if !alreadySet[f.Name] {
-			key := prefix + strings.ToUpper(strings.Replace(f.Name, "-", "_", -1))
-			val := os.Getenv(key)
-			if val != "" {
-				if serr := fs.Set(f.Name, val); serr != nil {
-					err = fmt.Errorf("invalid value %q for %s: %v", val, key, serr)
-				}
-			}
-		}
-	})
-	return err
+	envcfg.Parse()
 }
