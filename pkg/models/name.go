26eccffea2ff90fcdba5aed6bc53ad63c9fffbb7
liut <liutao@liut.cc>
2019-09-12 13:12:38 +0800

    允许使用"·"分隔姓名

--- pkg/models/name.go
+++ pkg/models/name.go
@@ -4,13 +4,16 @@ import (
 	"strings"
 )
 
-// SplitName 这个其实没有用
+// SplitName 分隔姓名，使用 "·" " "
 func SplitName(cn string) (sn, gn string) {
 	cn = strings.TrimSpace(cn)
 	if pos := strings.LastIndexByte(cn, ' '); pos > 0 {
 		return cn[pos+1:], cn[0:pos]
 	}
-	a := strings.Split(strings.Trim(cn, " "), "")
+	if a := strings.Split(cn, "·"); len(a) == 2 {
+		return a[0], a[1]
+	}
+	a := strings.Split(cn, "")
 	switch len(a) {
 	case 2:
 		return a[0], a[1]
