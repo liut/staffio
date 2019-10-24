34a204f3f678265cab369e0b8522e77d391ec830
liut <liutao@liut.cc>
2019-10-25 01:55:06 +0800

    + comments

--- database/staffio_4_schema_content.sql
+++ database/staffio_4_schema_content.sql
@@ -26,3 +26,16 @@ CREATE TABLE IF NOT EXISTS links
 
 CREATE INDEX IF NOT EXISTS idx_links_created ON links (created);
 CREATE INDEX IF NOT EXISTS idx_links_position ON links (position);
+
+
+CREATE TABLE IF NOT EXISTS comments
+(
+	id serial,
+	topic_type smallint NOT NULL DEFAULT 0, -- 1=article, 2=weekly_report
+	topic_id int NOT NULL DEFAULT 0,
+	content text NOT NULL DEFAULT '',
+	from_uid name NOT NULL DEFAULT '',
+	to_uid name NOT NULL DEFAULT '',
+	created timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+)
