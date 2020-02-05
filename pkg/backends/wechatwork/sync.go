5d9f4edc10e6d73474bbf58ad5df6f363d00965c
liut <liutao@liut.cc>
2020-02-05 19:42:25 +0800

    Add DeptFn and Fix log wechatwork

--- pkg/backends/wechatwork/sync.go
+++ pkg/backends/wechatwork/sync.go
@@ -2,7 +2,6 @@ package wechatwork
 
 import (
 	"fmt"
-	"log"
 	"sort"
 	"strings"
 
@@ -14,11 +13,17 @@ import (
 	"github.com/liut/staffio/pkg/settings"
 )
 
+type User = exwechat.User
+type Department = exwechat.Department
+type Departments = exwechat.Departments
+
 // Syncer ...
 type Syncer struct {
+	DeptFn    func(dept *exwechat.Department, idx int)
 	BulkFn    func(svc backends.Servicer, t *team.Team, staffs models.Staffs) error
 	WithTeam  bool
 	WithStaff bool
+	Output    bool
 
 	api *exwechat.API
 }
@@ -33,13 +38,16 @@ func SyncDepartment(action, uid string) {
 		if len(uid) > 0 {
 			user, err := s.api.GetUser(uid)
 			if err != nil {
-				log.Print(err)
+				logger().Infow("get user fail", "err", err)
 				return
 			}
-			fmt.Println(user)
+			if s.Output {
+				fmt.Println(user)
+			}
+
 			return
 		}
-		fmt.Println("empty uid")
+		logger().Infow("empty uid")
 		return
 	}
 	s.RunIt()
@@ -54,26 +62,32 @@ func (s *Syncer) RunIt() error {
 
 	departments, err := s.api.ListDepartment(1)
 	if err != nil {
-		log.Print(err)
+		logger().Infow("list department fail", "err", err)
 		return err
 	}
 	sort.Sort(departments)
 	// log.Printf("departments: %v", data)
 	svc := backends.NewService()
-	for _, dept := range departments {
-		fmt.Printf("%4d %4d %14s 	%8d\n", dept.Id, dept.ParentId, dept.Name, dept.Order)
+	for i, dept := range departments {
+		if s.Output {
+			fmt.Printf("%4d %4d %14s 	%8d\n", dept.Id, dept.ParentId, dept.Name, dept.Order)
+		}
+
+		if s.DeptFn != nil {
+			s.DeptFn(&dept, i)
+		}
 		team := DepartmentToTeam(&dept, departments)
 		var staffs models.Staffs
 
 		if s.WithStaff {
 			users, err := s.api.ListUser(dept.Id, false)
 			if err != nil {
-				log.Print(err)
+				logger().Infow("list user fail", "err", err)
 				return err
 			}
 			for _, val := range users {
 				if !val.IsActived() || !val.IsEnabled() {
-					log.Printf("user %s status %s, enabled %v", val.Name, val.Status, val.Enabled)
+					logger().Infow("user actived?", "name", val.Name, "status", val.Status, "enabled", val.Enabled)
 					continue
 				}
 				team.Members = append(team.Members, val.UID)
@@ -92,12 +106,12 @@ func (s *Syncer) RunIt() error {
 		if s.WithTeam && s.BulkFn != nil {
 			err = s.BulkFn(svc, team, staffs)
 			if err != nil {
-				log.Print(err)
+				logger().Infow("call bulkFn fail", "err", err)
 				return err
 			}
 		}
 
 	}
-	log.Print("all done")
+	logger().Infow("syncer ran all done")
 	return nil
 }
